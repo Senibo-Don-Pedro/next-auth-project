7a33b0093fef860d7c451528c069dd27a0f2cdae
Senibo Don-Pedro <senibodonpedro@gmail.com>
2024-12-06 16:14:55 +0000

    Started landing page

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button";
+
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+export default function Home() {
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-sky-500">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className,
+          )}
+        >
+          🔐Auth
+        </h1>
+        <p className="text-lg text-white">A full blown Authentication system</p>
+        <div>
+          
+        </div>
+      </div>
+    </main>
+  );
+}
