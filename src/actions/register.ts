9de85983e052bd3773998f48e400b63c2153ae8b
Senibo <senibodonpedro@gmail.com>
2024-12-08 22:36:22 +0100

    successfully connected to db

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -3,11 +3,35 @@
 import { RegisterShema } from "@/schemas";
 import { z } from "zod";
 
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
+
 export async function register(values: z.infer<typeof RegisterShema>) {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   const validatedFields = RegisterShema.safeParse(values);
 
-  if (!validatedFields) return { error: "Invalid fields!!" };
+  if (!validatedFields.success) return { error: "Invalid fields!!" };
 
-  return { success: "Email sent!!" };
+  const { email, password, name } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) return { error: "Email already in use" };
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // Todo: send verification email
+  return { success: "User Created!!" };
 }
